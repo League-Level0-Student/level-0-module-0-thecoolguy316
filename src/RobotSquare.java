79c247ab9230cbea1df17cfa95b4f91e32d32f7a
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-05-03 13:26:21 -0700

    Robot Graffiti, Morning Zombie

--- src/RobotSquare.java
+++ src/RobotSquare.java
@@ -11,22 +11,27 @@ public class RobotSquare {
     public static void main(String[] args) throws Exception {
  
     	// 1. Make a new Robot
-
+Robot cool = new Robot();
 
         // 3. Put the robot's pen down
 
-
+cool.penDown();
+cool.setRandomPenColor();
         // 6. Make the robot move as fast as possible
-
+cool.setSpeed(50);
 
         // 5. Do everything below here 4 times
 
-
+for (int i=0; i<100; i++) {
         //         2. Move your robot 200 pixels
 
-
+cool.move(200);
         //         4. Turn the robot 90 degrees to the right (90 degrees)
 
-
+cool.turn(90);
+cool.sparkle();
     }
+}
+{
+}
 }
---
+++ src/extra/RobotGrafitti.java
@@ -0,0 +1,20 @@
+package extra;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class RobotGrafitti {
+	public static void main(String[] args) throws Exception {
+//make a new robot
+	Robot look = new Robot();
+	look.miniaturize();
+	look.setSpeed(50);
+look.penDown();
+look.move(200);
+look.turn(180);
+look.move(100);
+look.turn(225);
+look.move(100);
+look.turn(90);
+look.move(100);
+}
+}
